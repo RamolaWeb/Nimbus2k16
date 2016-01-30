e5e0417cb586ab137b1bfc9686aea7702ba6037f
Ramola <sramola5@gmail.com>
2016-01-30 18:34:05 +0530

    ADDED EVENT DETAIL AND DEPARTMENTAL TEAM DETAIL

--- app/src/main/java/com/appteam/nimbus/DepartmentalTeam.java
+++ app/src/main/java/com/appteam/nimbus/DepartmentalTeam.java
@@ -11,7 +11,8 @@ import android.view.View;
 import java.util.ArrayList;
 
 public class DepartmentalTeam extends AppCompatActivity {
-private RecyclerView recyclerView;
+    private static final String KEY_NAME_TEAM = "Detail";
+    private RecyclerView recyclerView;
     private adapter_department adapter;
     private ArrayList<Item_department> list;
 
@@ -22,33 +23,35 @@ private RecyclerView recyclerView;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        list=setData();
-        recyclerView= (RecyclerView) findViewById(R.id.recycler_view_department);
+        list = setData();
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_department);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter=new adapter_department(this,list);
+        adapter = new adapter_department(this, list);
         recyclerView.setAdapter(adapter);
         recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, new RecyclerItemClickListener.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-startActivity(new Intent(DepartmentalTeam.this,Team.class));
+                Intent i=new Intent(DepartmentalTeam.this, Team.class);
+                i.putExtra(KEY_NAME_TEAM,list.get(position).name);
+                startActivity(i);
             }
         }));
     }
 
     private ArrayList<Item_department> setData() {
-        ArrayList<Item_department> itemlist=new ArrayList<>();
-        itemlist.add(new Item_department("ECE",R.drawable.placeholder_image));
-        itemlist.add(new Item_department("ELECTRICAL",R.drawable.placeholder_image));
-        itemlist.add(new Item_department("CIVIL",R.drawable.placeholder_image));
-        itemlist.add(new Item_department("MECHANICAL",R.drawable.placeholder_image));
-        itemlist.add(new Item_department("CSE",R.drawable.placeholder_image));
-        itemlist.add(new Item_department("CHEMICAL",R.drawable.placeholder_image));
+        ArrayList<Item_department> itemlist = new ArrayList<>();
+        itemlist.add(new Item_department("ECE", R.drawable.placeholder_image));
+        itemlist.add(new Item_department("ELECTRICAL", R.drawable.placeholder_image));
+        itemlist.add(new Item_department("CIVIL", R.drawable.placeholder_image));
+        itemlist.add(new Item_department("MECHANICAL", R.drawable.placeholder_image));
+        itemlist.add(new Item_department("CSE", R.drawable.placeholder_image));
+        itemlist.add(new Item_department("CHEMICAL", R.drawable.placeholder_image));
         return itemlist;
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        overridePendingTransition(R.anim.close_main,R.anim.close_next);
+        overridePendingTransition(R.anim.close_main, R.anim.close_next);
     }
 }
--- app/src/main/java/com/appteam/nimbus/Team.java
+++ app/src/main/java/com/appteam/nimbus/Team.java
@@ -1,11 +1,19 @@
 package com.appteam.nimbus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class Team extends AppCompatActivity {
-
+    private static final String KEY_NAME_TEAM ="Detail" ;
+    private String team_name;
+    private event_adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,8 +21,30 @@ public class Team extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Team");
+        Intent i=getIntent();
+        if(i!=null){
 
+            team_name=i.getStringExtra(KEY_NAME_TEAM).toLowerCase();
+            getSupportActionBar().setTitle(team_name.toUpperCase());
+        }
+        View teamdetail=findViewById(R.id.team_detail);
+        TextView description= (TextView) teamdetail.findViewById(R.id.team_detail_description);
+        int id=getResources().getIdentifier(team_name,"string",this.getPackageName());
+        String content=getResources().getString(id);
+        description.setText(content);
+
+        View contact_detail=findViewById(R.id.contact_detail);
+        TextView contact_description= (TextView) contact_detail.findViewById(R.id.contact_detail_description);
+        int id1=getResources().getIdentifier(team_name+"_contact","string",this.getPackageName());
+        String content_contact=getResources().getString(id1);
+        contact_description.setText(content_contact);
+
+       RecyclerView recyclerView= (RecyclerView) findViewById(R.id.recycler_view_team_event);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+int id3=getResources().getIdentifier(team_name+"_event","array",this.getPackageName());
+     String list[]=  getResources().getStringArray(id3);
+        adapter=new event_adapter(this,list);
+        recyclerView.setAdapter(adapter);
     }
 
 }
